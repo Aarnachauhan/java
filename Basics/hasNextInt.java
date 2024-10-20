01a1221289baaa987e50aea18fdeb5934754dae7
Aarna chauhan <chauhanaarna6@gmail.com>
2024-10-20 18:46:05 +0530

    Create hasNextInt.java

---
+++ Basics/hasNextInt.java
@@ -0,0 +1,12 @@
+
+//to check if the number entered by the user is of int data type or not
+
+
+import java.util.Scanner;
+class Marks{
+ public static void main(String[] args){
+     Scanner sc=new Scanner(System.in);
+     System.out.println(sc.hasNextInt());
+     
+ }
+} 
